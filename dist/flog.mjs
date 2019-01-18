a9b24ab3ff20c499f22a476b4f4ed781d68b15ea
Warwick Grigg <warwick.grigg@gmail.com>
2019-01-18 18:54:01 +0000

    bundling

--- dist/flog.mjs
+++ dist/flog.mjs
@@ -1,8 +1,29 @@
-exports.flog = logger => transformer => threshold => level => text => data => {
-  if (level > threshold) {
-    logger(text, transformer ? transformer(data) : data);
-  }
-  return data;
+var flog = function flog(logger) {
+  return function (transformer) {
+    return function (threshold) {
+      return function (level) {
+        return function (text) {
+          return function (data) {
+            if (level > threshold) {
+              logger(text, transformer ? transformer(data) : data);
+            }
+
+            return data;
+          };
+        };
+      };
+    };
+  };
 };
-exports.toJSON = data => JSON.stringify(data, null, 2);
+
+var toJSON = function toJSON(data) {
+  return JSON.stringify(data, null, 2);
+};
+
+var flog$1 = {
+  flog: flog,
+  toJSON: toJSON
+};
+
+export default flog$1;
 //# sourceMappingURL=flog.mjs.map
--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
+  /*
   input: 'src/flog.js',
   output: {
     file: 'dist/flog.umd.js',
     format: 'umd'
   },
+  */
   plugins: [
     resolve(),
     babel({
